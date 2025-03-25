1875fd6dc2c6b2cc203e7675422f35abfc8cd0b0
Kiranraj Muthuraj <kiranrajofficial.de@yahoo.com>
2025-03-25 12:42:27 +0100

    Renamed for thread in hole dataset

--- thread_in_hole_v2/thread_in_hole_v2_dataset_builder.py
+++ thread_in_hole_v2/thread_in_hole_v2_dataset_builder.py
@@ -7,7 +7,7 @@ import tensorflow_datasets as tfds
 import tensorflow_hub as hub
 
 
-class ExampleDataset(tfds.core.GeneratorBasedBuilder):
+class ThreadInHoleV2(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for example dataset."""
 
     VERSION = tfds.core.Version('1.0.0')
